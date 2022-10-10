1d3d8b7a4429782c4d43618bed8c7009ced5cdcb
Dsposeidon <51982814+Dsposeidon@users.noreply.github.com>
2022-10-10 14:36:23 -0400

    Add files via upload

---
+++ #5 for course-zoomcamp/predict.py
@@ -0,0 +1,84 @@
+
+customer = {
+    'gender': 'female',
+    'seniorcitizen': 0,
+    'partner': 'yes',
+    'dependents': 'no',
+    'phoneservice': 'no',
+    'multiplelines': 'no_phone_service',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'no',
+    'onlinebackup': 'yes',
+    'deviceprotection': 'no',
+    'techsupport': 'no',
+    'streamingtv': 'no',
+    'streamingmovies': 'no',
+    'contract': 'month-to-month',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'electronic_check',
+    'tenure': 1,
+    'monthlycharges': 29.85,
+    'totalcharges': 29.85
+}
+
+
+
+X = dv.transform([customer])
+
+
+
+
+model.predict_proba(X)[0,1]
+
+
+
+
+y_pred = model.predict_proba(X)[0,1]
+
+
+
+
+print('input:', customer)
+print('output:', y_pred)
+
+
+
+
+
+
+
+url = 'http://localhost:8080/predict'
+
+
+
+
+customer = {
+    'gender': 'female',
+    'seniorcitizen': 0,
+    'partner': 'yes',
+    'dependents': 'no',
+    'phoneservice': 'no',
+    'multiplelines': 'no_phone_service',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'no',
+    'onlinebackup': 'yes',
+    'deviceprotection': 'no',
+    'techsupport': 'no',
+    'streamingtv': 'no',
+    'streamingmovies': 'no',
+    'contract': 'two_year',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'electronic_check',
+    'tenure': 1,
+    'monthlycharges': 29.85,
+    'totalcharges': 29.85
+}
+
+
+
+response = requests.post(url, json=customer).json()
+
+
+
+
+response
